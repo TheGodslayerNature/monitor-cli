a448628b976b81731cb45e50961223f6955ce28d
Alexandre Da Silva <alexandrerider@hotmail.com>
2024-12-03 16:55:53 -0300

    First commit

---
+++ src/main/java/com/fate/monitor_cli/MonitorCliApplication.java
@@ -0,0 +1,13 @@
+package com.fate.monitor_cli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MonitorCliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MonitorCliApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/fate/monitor_cli/MonitorCliApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fate.monitor_cli;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MonitorCliApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
