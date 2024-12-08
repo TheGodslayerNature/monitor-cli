d5312f593f26547669304bdadad57694a2234d61
Alexandre Da Silva <alexandrerider@hotmail.com>
2024-12-08 20:11:53 -0300

    Add health monitoring with schedule, enable check cpu usage, memory usage and little info about the disk.

--- src/main/java/com/fate/monitor_cli/SystemHealthMonitor.java
+++ src/main/java/com/fate/monitor_cli/SystemHealthMonitor.java
@@ -3,11 +3,18 @@ package com.fate.monitor_cli;
 import com.fate.monitor_cli.service.SystemMonitor;
 import org.springframework.shell.standard.ShellComponent;
 import org.springframework.shell.standard.ShellMethod;
+import org.springframework.shell.standard.ShellOption;
+
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 @ShellComponent
 public class SystemHealthMonitor {
 
     private final SystemMonitor systemMonitor;
+    private final ScheduledExecutorService scheduledExecutorService = Executors.newSingleThreadScheduledExecutor();
+    private boolean monitoring = false;
 
     public SystemHealthMonitor(SystemMonitor systemMonitor) {
         this.systemMonitor = systemMonitor;
@@ -34,6 +41,11 @@ public class SystemHealthMonitor {
         return String.format("Memory Available: %.2f", systemMonitor.getTotalMemoryAvailable());
     }
 
+    @ShellMethod(key = "usage-m")
+    public String memoryPercent() {
+        return String.format("Memory usage: %.2f", systemMonitor.getMemoryUsagePercent());
+    }
+
     @ShellMethod(key = "status-m")
     public String memoryStatus(){
         double total = systemMonitor.getTotalMemory();
@@ -47,5 +59,42 @@ public class SystemHealthMonitor {
     @ShellMethod(key = "status-d")
     public String diskInfo() {
         return systemMonitor.disksInfo();
+    }
+
+    @ShellMethod(value = "Starts dynamic system monitoring", key = "start-m")
+    public void SystemMonitoring(@ShellOption(defaultValue = "8") long interval){
+        if (monitoring){
+            return;
+        }
+        monitoring = true;
+
+        if (checkMonitoringInterval(interval)){
+            System.out.println("interval cannot be less than 8 seconds");
+            return;
+        }
+
+        scheduledExecutorService.scheduleAtFixedRate( () -> {
+            System.out.println("\n=== Health Monitoring ===");
+            System.out.printf("%-20s %-10s%n", "Métrica", "Valor");
+            System.out.printf("%-20s %-10s%n", "CPU Usage", String.format("%.2f",systemMonitor.getCpuUsage()) + "%");
+            System.out.printf("%-20s %-10s%n", "Memory Usage", String.format("%.2f", systemMonitor.getMemoryUsagePercent()));
+            System.out.println("Disk information\n");
+            System.out.println(systemMonitor.disksInfo());
+        },0,interval, TimeUnit.SECONDS);
+    }
+
+    private boolean checkMonitoringInterval(long interval){
+        return interval < 8;
+    }
+
+    @ShellMethod(value = "Stop dynamic monitoring", key = "stop-m")
+    public void stopMonitoring(){
+        if (!monitoring){
+            System.out.println("Monitoring is not active");
+            return;
+        }
+        monitoring = false;
+        scheduledExecutorService.shutdown();
+        System.out.println("Monitoring stopped");
     }
 }
